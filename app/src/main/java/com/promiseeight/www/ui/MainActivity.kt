862ca1f9157e72833fb16de5dd02365ba3da5c0e
Jung SeokJoon <eshc.joon@gmail.com>
2023-01-22 14:13:12 +0900

    [mod] #2 - MainActivity - ui package로 이동

--- app/src/main/java/com/promiseeight/www/ui/MainActivity.kt
+++ app/src/main/java/com/promiseeight/www/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.promiseeight.www
+package com.promiseeight.www.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.promiseeight.www.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
