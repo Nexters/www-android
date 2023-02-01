ba9164a80ea8382f5364a6e67ba7638cb3af83ab
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-02 07:58:11 +0900

    [add] #9 - BaseFragment 추가
    Binding 사용을 위한 BaseFragment
    
    참고 사이트 : https://android-dev.tistory.com/entry/AndroidKotlin-%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C-ViewBinding-with-BaseActivity-BaseFragment

---
+++ app/src/main/java/com/promiseeight/www/ui/common/BaseFragment.kt
@@ -0,0 +1,38 @@
+package com.promiseeight.www.ui.common
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+/*
+    동일한 구조의 Fragment가 많을 것 같아 BaseFragment 추가했습니다.
+*/
+abstract class BaseFragment<B: ViewDataBinding> : Fragment() {
+    private var _binding: B? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = getFragmentBinding(inflater, container)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    abstract fun getFragmentBinding(inflater: LayoutInflater, container: ViewGroup?): B
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
