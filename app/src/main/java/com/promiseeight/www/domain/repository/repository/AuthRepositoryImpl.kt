bcbdb195bf4dd19dcdef82d126847cd4a03936e9
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-02-04 03:00:56 +0900

    [chore] #6 Presenters 추가

---
+++ app/src/main/java/com/promiseeight/www/domain/repository/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,6 @@
+package com.promiseeight.www.domain.repository.repository
+
+import com.promiseeight.www.domain.repository.AuthRepository
+
+class AuthRepositoryImpl : AuthRepository {
+}
---
+++ app/src/main/java/com/promiseeight/www/domain/repository/repository/MeetingRepositoryImpl.kt
@@ -0,0 +1,6 @@
+package com.promiseeight.www.domain.repository.repository
+
+import com.promiseeight.www.domain.repository.MeetingRepository
+
+class MeetingRepositoryImpl : MeetingRepository {
+}
