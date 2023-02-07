312d14a7485d447167936df0c0829e854d1e7306
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-08 08:53:43 +0900

    [mod] #13 - MeetingInfo...Fragment 수정

--- app/src/main/java/com/promiseeight/www/ui/meeting/info/MeetingInfoDateFragment.kt
+++ app/src/main/java/com/promiseeight/www/ui/meeting/info/MeetingInfoDateFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.promiseeight.www.R
 import com.promiseeight.www.databinding.FragmentMeetingInfoDateBinding
 import com.promiseeight.www.databinding.FragmentMeetingInfoPeriodBinding
+import com.promiseeight.www.ui.common.AddNavHostFragment
 import com.promiseeight.www.ui.common.BaseFragment
+import com.promiseeight.www.ui.common.JoinNavHostFragment
 
 class MeetingInfoDateFragment : BaseFragment<FragmentMeetingInfoDateBinding>() {
     override fun getFragmentBinding(
@@ -20,9 +23,19 @@ class MeetingInfoDateFragment : BaseFragment<FragmentMeetingInfoDateBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.btnNext.setOnClickListener {
-            findNavController().navigate(
-                MeetingInfoDateFragmentDirections.actionFragmentMeetingInfoDateToFragmentMeetingInfoPlace()
-            )
+            when (parentFragment) {
+                is JoinNavHostFragment -> {
+                    findNavController().navigate(
+                        R.id.action_fragment_join_meeting_info_date_to_fragment_join_meeting_info_place
+                    )
+                }
+                is AddNavHostFragment -> {
+                    findNavController().navigate(
+                        R.id.action_fragment_add_meeting_info_date_to_fragment_add_meeting_info_place
+                    )
+                }
+
+            }
         }
     }
 }
