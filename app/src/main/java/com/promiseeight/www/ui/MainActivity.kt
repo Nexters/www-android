ec15d9aeabaa1aef85e74e76a1e84960a50a6e7d
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-13 21:00:10 +0900

    [mod] #21 - MainActivity, HomeFragment - AndroidEntryPoint 추가

--- app/src/main/java/com/promiseeight/www/ui/MainActivity.kt
+++ app/src/main/java/com/promiseeight/www/ui/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.promiseeight.www.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.promiseeight.www.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
