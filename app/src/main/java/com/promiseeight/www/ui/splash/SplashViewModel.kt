6bd1d25a74e4f6750ccc9ce16966c0fcee036979
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-13 20:58:47 +0900

    [feat] #21 - SplashFragment, SplashViewModel 추가

---
+++ app/src/main/java/com/promiseeight/www/ui/splash/SplashViewModel.kt
@@ -0,0 +1,35 @@
+package com.promiseeight.www.ui.splash
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.promiseeight.www.domain.usecase.auth.GetAccessTokenWithFcmTokenUseCase
+import com.promiseeight.www.domain.usecase.auth.IsAccessTokenInDeviceUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    private val getAccessTokenWithFcmTokenUseCase: GetAccessTokenWithFcmTokenUseCase,
+    isAccessTokenInDeviceUseCase: IsAccessTokenInDeviceUseCase
+) : ViewModel() {
+
+    val isAccessTokenInDevice = isAccessTokenInDeviceUseCase()
+
+    private var _splashStatus = MutableStateFlow<SplashStatus>(SplashStatus.READY)
+    val splashStatus : StateFlow<SplashStatus> get() = _splashStatus
+
+    fun getAccessTokenWithFcmToken(fcmToken: String) {
+        viewModelScope.launch {
+            getAccessTokenWithFcmTokenUseCase(fcmToken)
+                .onSuccess {
+                    _splashStatus.value = SplashStatus.SUCCESS
+                }.onFailure {
+                    _splashStatus.value = SplashStatus.ERROR
+                }
+        }
+    }
+
+}
