ea70468aaba35f832712fc5d16bd0023afdb7e07
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-13 20:58:07 +0900

    [add] #21 - Application 추가

---
+++ app/src/main/java/com/promiseeight/www/App.kt
@@ -0,0 +1,13 @@
+package com.promiseeight.www
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
