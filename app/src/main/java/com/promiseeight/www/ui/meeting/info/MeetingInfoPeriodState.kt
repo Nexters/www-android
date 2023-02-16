f223a4cef69bf672eebeade5bbe8d549594d10bc
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-16 22:47:48 +0900

    [add] #7 - MeetingInfoPeriodState 추가 및 MeetingInfoPeriodFragment 수정

---
+++ app/src/main/java/com/promiseeight/www/ui/meeting/info/MeetingInfoPeriodState.kt
@@ -0,0 +1,8 @@
+package com.promiseeight.www.ui.meeting.info
+
+import com.promiseeight.www.ui.model.CalendarUiModel
+
+data class MeetingInfoPeriodState(
+    val meetingPeriodStart : CalendarUiModel? = null,
+    val meetingPeriodEnd : CalendarUiModel? = null
+)
