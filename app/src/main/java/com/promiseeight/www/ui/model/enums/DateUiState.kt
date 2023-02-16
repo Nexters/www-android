068d99f4ba6ea60afda3af0ce3b5ebcb403c068f
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-16 09:01:33 +0900

    [add] #7 - CalendarUiModel, DateUiState 추가

---
+++ app/src/main/java/com/promiseeight/www/ui/model/enums/DateUiState.kt
@@ -0,0 +1,15 @@
+package com.promiseeight.www.ui.model.enums
+
+enum class DateUiState {
+    INITIAL,
+    SELECTED,
+    SELECTED_START,
+    SELECTED_END,
+    PASS,
+    PASS_START,
+    PASS_END,
+    GRAD_START,
+    GRAD_END,
+    GRAD_SELECT_START,
+    GRAD_SELECT_END
+}
