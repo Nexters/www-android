9443abdefeb012f056f333862186a60101927e6b
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-17 09:04:13 +0900

    [add] #24 - MeetingInfoPlaceFragment, MeetingShareFragment - argument 값 저장, 받아오기 기능 추가

--- app/src/main/java/com/promiseeight/www/ui/meeting/MeetingShareFragment.kt
+++ app/src/main/java/com/promiseeight/www/ui/meeting/MeetingShareFragment.kt
@@ -1,15 +1,30 @@
 package com.promiseeight.www.ui.meeting
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.navArgs
 import com.promiseeight.www.databinding.FragmentMeetingShareBinding
 import com.promiseeight.www.ui.common.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MeetingShareFragment : BaseFragment<FragmentMeetingShareBinding>() {
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentMeetingShareBinding {
         return FragmentMeetingShareBinding.inflate(inflater,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //확인을 위한 Toast, 삭제 예정
+        navArgs<MeetingShareFragmentArgs>().value.run {
+            Toast.makeText(requireContext(),"초대 코드 : $argInvitationCode 링크 : $argInvitationLink",Toast.LENGTH_SHORT).show()
+        }
     }
 }
