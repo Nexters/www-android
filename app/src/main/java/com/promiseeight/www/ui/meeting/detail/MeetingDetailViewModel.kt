b29b92d673b6a0619d7a47ed9766c6e7b03aaa90
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-18 23:42:26 +0900

    [chore] #19 - dummy 수정

--- app/src/main/java/com/promiseeight/www/ui/meeting/detail/MeetingDetailViewModel.kt
+++ app/src/main/java/com/promiseeight/www/ui/meeting/detail/MeetingDetailViewModel.kt
@@ -6,15 +6,15 @@ import com.promiseeight.www.ui.model.PlaceRankUiModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class MeetingDetailViewModel (
+class MeetingDetailViewModel(
 
 ) : ViewModel() {
 
     private val _dateRanks = MutableStateFlow(listOf<DateRankUiModel>())
-    val dateRanks : StateFlow<List<DateRankUiModel>> get() = _dateRanks
+    val dateRanks: StateFlow<List<DateRankUiModel>> get() = _dateRanks
 
     private val _placeRanks = MutableStateFlow(listOf<PlaceRankUiModel>())
-    val placeRanks : StateFlow<List<PlaceRankUiModel>> get() = _placeRanks
+    val placeRanks: StateFlow<List<PlaceRankUiModel>> get() = _placeRanks
 
     init {
         _dateRanks.value = dateDummy
@@ -30,29 +30,6 @@ val dateDummy = listOf(
         time = "아침",
         count = 3,
         progress = 100 * 3 / 5
-    ),
-    DateRankUiModel(
-        id = 2,
-        ranking = 2,
-        date = "23.01.21",
-        time = "아침",
-        count = 3,
-        progress = 100 * 3 / 5
-    ),
-    DateRankUiModel(
-        id = 3,
-        ranking = 2,
-        date = "23.01.19",
-        time = "점심",
-        count = 3,
-        progress = 100 * 3 / 5
-    ),DateRankUiModel(
-        id = 4,
-        ranking = 3,
-        date = "23.01.22",
-        time = "아침",
-        count = 2,
-        progress = 100 * 2 / 5
     )
 )
 
@@ -63,82 +40,5 @@ val placeDummy = listOf(
         name = "강남역",
         count = 3,
         progress = 100 * 3 / 5
-    ),
-    PlaceRankUiModel(
-        id = 2,
-        ranking = 2,
-        name = "내방역",
-        count = 3,
-        progress = 100 * 3 / 5
-    ),
-    PlaceRankUiModel(
-        id = 3,
-        ranking = 3,
-        name = "이수역",
-        count = 0,
-        progress = 100 * 0 / 5
-    ),
-    PlaceRankUiModel(
-        id = 4,
-        ranking = 2,
-        name = "강남역",
-        count = 3,
-        progress = 100 * 3 / 5
-    ),
-    PlaceRankUiModel(
-        id = 5,
-        ranking = 2,
-        name = "내방역",
-        count = 3,
-        progress = 100 * 3 / 5
-    ),
-    PlaceRankUiModel(
-        id = 6,
-        ranking = 3,
-        name = "이수역",
-        count = 0,
-        progress = 100 * 0 / 5
-    ),
-    PlaceRankUiModel(
-        id = 7,
-        ranking = 2,
-        name = "강남역",
-        count = 3,
-        progress = 100 * 3 / 5
-    ),
-    PlaceRankUiModel(
-        id = 8,
-        ranking = 2,
-        name = "내방역",
-        count = 3,
-        progress = 100 * 3 / 5
-    ),
-    PlaceRankUiModel(
-        id = 9,
-        ranking = 3,
-        name = "이수역",
-        count = 0,
-        progress = 100 * 0 / 5
-    ),
-    PlaceRankUiModel(
-        id = 12,
-        ranking = 2,
-        name = "강남역",
-        count = 3,
-        progress = 100 * 3 / 5
-    ),
-    PlaceRankUiModel(
-        id = 18,
-        ranking = 2,
-        name = "내방역",
-        count = 3,
-        progress = 100 * 3 / 5
-    ),
-    PlaceRankUiModel(
-        id = 19,
-        ranking = 3,
-        name = "이수역",
-        count = 0,
-        progress = 100 * 0 / 5
     )
 )
