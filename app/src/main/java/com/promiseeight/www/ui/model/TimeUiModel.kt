796ce5111a4933dca9eef0f9e0927959edc1c5c9
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-22 07:49:37 +0900

    [add] #8 - 날짜,시간 선택 뷰 - TimeUiModel 추가

---
+++ app/src/main/java/com/promiseeight/www/ui/model/TimeUiModel.kt
@@ -0,0 +1,9 @@
+package com.promiseeight.www.ui.model
+
+data class TimeUiModel(
+    val id : Int,
+    val date : String,
+    val time : String,
+    val selected : Boolean,
+    val disabled : Boolean
+)
