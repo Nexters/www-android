c670752c51b866c6b048fffba449118b5067fcb8
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-22 23:47:33 +0900

    [add] #19 - MeetingDetailConfirm,Vote Fragment 추가

---
+++ app/src/main/java/com/promiseeight/www/ui/meeting/detail/MeetingDetailConfirmFragment.kt
@@ -0,0 +1,68 @@
+package com.promiseeight.www.ui.meeting.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.navGraphViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.promiseeight.www.R
+import com.promiseeight.www.databinding.FragmentMeetingDetailRankBinding
+import com.promiseeight.www.ui.adapter.RankAdapter
+import com.promiseeight.www.ui.common.BaseFragment
+import com.promiseeight.www.ui.model.PlaceRankUiModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class MeetingDetailConfirmFragment : BaseFragment<FragmentMeetingDetailRankBinding>() {
+
+    private val viewModel : MeetingDetailViewModel by navGraphViewModels(R.id.main_navigation)
+
+    private val rankAdapter: RankAdapter<PlaceRankUiModel> by lazy {
+        RankAdapter()
+    }
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentMeetingDetailRankBinding {
+        return FragmentMeetingDetailRankBinding.inflate(inflater,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.let {
+            initRecyclerView(it.rvRank)
+        }
+
+        initObserver()
+    }
+
+    private fun initRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.run {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            adapter = rankAdapter
+        }
+
+
+    }
+
+    private fun initObserver() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
+                launch {
+                    viewModel.placeRanks.collectLatest {
+                        rankAdapter.submitList(it)
+                    }
+                }
+            }
+        }
+    }
+}
