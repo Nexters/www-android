47ed2e850ba56a595de46c23718764e4767d9447
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-22 21:44:20 +0900

    [add] #12 - HomeMeetingAdapter - onClick 추가

--- app/src/main/java/com/promiseeight/www/ui/adapter/HomeMeetingAdapter.kt
+++ app/src/main/java/com/promiseeight/www/ui/adapter/HomeMeetingAdapter.kt
@@ -8,14 +8,18 @@ import androidx.recyclerview.widget.RecyclerView
 import com.promiseeight.www.databinding.ItemHomeMeetingBinding
 import com.promiseeight.www.ui.model.MeetingUiModel
 
-class HomeMeetingAdapter : ListAdapter<MeetingUiModel,HomeMeetingAdapter.HomeMeetingViewHolder>(HomeMeetingDiffCallback()) {
+class HomeMeetingAdapter(
+    private val onClick : (MeetingUiModel) -> Unit
+) : ListAdapter<MeetingUiModel,HomeMeetingAdapter.HomeMeetingViewHolder>(HomeMeetingDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeMeetingViewHolder {
         return HomeMeetingViewHolder(
-            ItemHomeMeetingBinding.inflate(
+            binding = ItemHomeMeetingBinding.inflate(
                 LayoutInflater.from(parent.context), parent, false
             )
-        )
+        ){ position ->
+            onClick(currentList[position])
+        }
     }
 
     override fun onBindViewHolder(holder: HomeMeetingViewHolder, position: Int) {
@@ -23,8 +27,15 @@ class HomeMeetingAdapter : ListAdapter<MeetingUiModel,HomeMeetingAdapter.HomeMee
     }
 
     class HomeMeetingViewHolder(
-        val binding : ItemHomeMeetingBinding
+        private val binding : ItemHomeMeetingBinding,
+        private val onClicked : (Int) -> Unit
     ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.root.setOnClickListener {
+                onClicked(adapterPosition)
+            }
+        }
 
         fun bind(homeMeeting : MeetingUiModel) {
             binding.homeMeeting = homeMeeting
