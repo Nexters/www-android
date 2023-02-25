c732fb49bbd6a2fadd430e34e169055fd738d04a
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-25 11:20:03 +0900

    [mod] #8 - ui model 수정

--- app/src/main/java/com/promiseeight/www/ui/model/CandidateUiModel.kt
+++ app/src/main/java/com/promiseeight/www/ui/model/CandidateUiModel.kt
@@ -1,6 +1,7 @@
 package com.promiseeight.www.ui.model
 
 data class CandidateUiModel(
+    var id : Long = 0L,
     val title : String,
     val isPossibleDelete : Boolean = true
 )
--- app/src/main/java/com/promiseeight/www/ui/model/TimeUiModel.kt
+++ app/src/main/java/com/promiseeight/www/ui/model/TimeUiModel.kt
@@ -1,9 +1,12 @@
 package com.promiseeight.www.ui.model
 
+import com.promiseeight.www.domain.model.PromiseTime
+import org.joda.time.DateTime
+
 data class TimeUiModel(
-    val id : Int,
-    val date : String,
-    val time : String,
+    val id : Long,
+    val date : DateTime,
+    val time : PromiseTime,
     val selected : Boolean,
     val disabled : Boolean
 )
