82330117b3d935c9e2a329eb98e05065ca14cd33
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-27 07:35:43 +0900

    [mod] #45 - 에러 문구 수정

--- app/src/main/java/com/promiseeight/www/data/model/exception/WwwException.kt
+++ app/src/main/java/com/promiseeight/www/data/model/exception/WwwException.kt
@@ -2,13 +2,13 @@ package com.promiseeight.www.data.model.exception
 
 fun getWwwException(code : Int) : Exception {
     return when(code) {
-        403 -> Exception("접근이 거부되었습니다.")
-        500 -> Exception("서버에서 에러가 발생하였습니다.")
-        1000 -> Exception("서버 요청 중 에러가 발생하였습니다.")
-        3001 -> Exception("이미 참여한 약속방입니다.")
-        4000 -> Exception("방이 존재하지 않습니다.")
-        4001 -> Exception("존재하지 않는 유저입니다.")
-        5000 -> Exception("이미 투표가 시작된 방입니다.")
-        else -> Exception("에러가 발생하였습니다.")
+        403 -> Exception("접근이 거부되었어요")
+        500 -> Exception("서버에서 에러가 발생했어요")
+        1000 -> Exception("서버 요청 중 에러가 발생했어요")
+        3001 -> Exception("이미 참여한 약속방이에요")
+        4000 -> Exception("존재하지 않는 방이에요")
+        4001 -> Exception("존재하지 않는 유저에요")
+        5000 -> Exception("이미 투표가 시작된 방이에요")
+        else -> Exception("에러가 발생하였어요")
     }
 }
