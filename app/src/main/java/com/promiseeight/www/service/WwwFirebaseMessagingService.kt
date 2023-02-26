fd8cfd2df12d51ea608a6f694bb675e399220d73
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-26 10:55:33 +0900

    [mod] #40 - WwwFirebaseMessagingService 수정

--- app/src/main/java/com/promiseeight/www/service/WwwFirebaseMessagingService.kt
+++ app/src/main/java/com/promiseeight/www/service/WwwFirebaseMessagingService.kt
@@ -3,10 +3,16 @@ package com.promiseeight.www.service
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import com.promiseeight.www.notification.WwwNotificationManager.createNotification
+import com.promiseeight.www.notification.WwwNotificationModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import org.json.JSONObject
+import timber.log.Timber
 
 @AndroidEntryPoint
 class WwwFirebaseMessagingService: FirebaseMessagingService()  {
@@ -27,7 +33,17 @@ class WwwFirebaseMessagingService: FirebaseMessagingService()  {
 
         // Check if message contains a data payload.
         if (remoteMessage.data.isNotEmpty()) {
-            Log.d("FirebaseToken", "Message data payload: ${remoteMessage.data}")
+            remoteMessage.data.get("data")?.let {
+                try {
+                    val data = Gson().fromJson(it,WwwNotificationModel::class.java)
+                    createNotification(context = this,data)
+                    Timber.d("asdasd ${data.contentType}")
+                } catch (e : Exception){
+                    Timber.d("WwwException : onMessageReceived ")
+                }
+
+            }
+
         }
 
         // Check if message contains a notification payload.
