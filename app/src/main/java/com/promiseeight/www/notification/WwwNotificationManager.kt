cc26a5123ed6a538838f6a974c976dfa8012be47
Jung SeokJoon <eshc.joon@gmail.com>
2023-02-26 10:55:13 +0900

    [mod] #40 - WwwNotificationManager 수정

--- app/src/main/java/com/promiseeight/www/notification/WwwNotificationManager.kt
+++ app/src/main/java/com/promiseeight/www/notification/WwwNotificationManager.kt
@@ -7,21 +7,20 @@ import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.navigation.NavDeepLinkBuilder
 import com.promiseeight.www.R
 
 object WwwNotificationManager {
     var index = 1
-    fun createNotification(context: Context) {
+    fun createNotification(context: Context, wwwNotificationData : WwwNotificationModel) {
         val notificationManager : NotificationManager
-            = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         createNotificationChannel(context,notificationManager)
 
         notificationManager.notify(
-            index++, buildNotification(
-                context,R.id.fragment_home
+            index, buildNotification(
+                context,wwwNotificationData
             )
         )
     }
@@ -35,25 +34,45 @@ object WwwNotificationManager {
             ).apply {
                 description = context.getString(R.string.www_notification_channel_description)
             }
+            notificationManager.createNotificationChannel(channel)
         }
     }
 
     private fun buildNotification(
         context : Context,
-        destinationId : Int,
-        argument : Bundle? = null
+        wwwNotificationData : WwwNotificationModel
     ) : Notification {
         val pendingIntent = NavDeepLinkBuilder(context)
             .setGraph(R.navigation.main_navigation)
-            .setDestination(destinationId)
-            .setArguments(argument)
+            .setDestination(getDestinationIdFromContentType(wwwNotificationData.contentType))
+            .setArguments(getArgument(wwwNotificationData))
             .createPendingIntent()
 
         return NotificationCompat.Builder(context, CHANNEL_ID)
             .setContentIntent(pendingIntent)
-            .setContentTitle("WWW")
-            .setContentText("WWW 알림 테스트입니다.")
+            .setContentTitle(wwwNotificationData.title)
+            .setContentText(wwwNotificationData.text)
+            .setSmallIcon(R.drawable.ic_app_logo_round)
+            .setAutoCancel(true)
             .build()
+    }
+
+    private fun getDestinationIdFromContentType(contentType : ContentType) : Int {
+        return when(contentType){
+            ContentType.MEETING -> R.id.fragment_meeting_detail
+            else -> R.id.fragment_meeting_detail
+        }
+    }
+
+    private fun getArgument(wwwNotificationData: WwwNotificationModel) : Bundle {
+        return when(wwwNotificationData.contentType){
+            ContentType.MEETING -> Bundle().apply {
+                putString("meetingId",wwwNotificationData.contentId.toString())
+            }
+            else -> Bundle().apply {
+                putString("meetingId",wwwNotificationData.contentId.toString())
+            }
+        }
     }
 
     private const val CHANNEL_ID = "www_notification_push_channel"
