00dcf85de147418aac201abf965fe4885bf2feb2
Jung SeokJoon <eshc.joon@gmail.com>
2023-03-01 08:02:34 +0900

    [mod] #48 #24 장소 투표 - domain 계층 추가

---
+++ app/src/main/java/com/promiseeight/www/domain/usecase/meeting/VotePlacesUseCase.kt
@@ -0,0 +1,14 @@
+package com.promiseeight.www.domain.usecase.meeting
+
+import com.promiseeight.www.domain.repository.MeetingRepository
+import com.promiseeight.www.ui.meeting.detail.MeetingStatus
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class VotePlacesUseCase @Inject constructor(
+    private val meetingRepository: MeetingRepository
+) {
+    operator fun invoke(meetingId : Long, placeIdList : List<Long>) : Flow<Result<Unit>> {
+        return meetingRepository.votePlaces(meetingId,placeIdList)
+    }
+}
