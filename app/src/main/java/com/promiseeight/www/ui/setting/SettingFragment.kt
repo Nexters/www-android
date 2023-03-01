d5278edfae54a1913a1a392b9cdb46ce2ede0cdb
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-03-01 16:41:33 +0900

    [add] openSource + uriParse

--- app/src/main/java/com/promiseeight/www/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/promiseeight/www/ui/setting/SettingFragment.kt
@@ -1,6 +1,8 @@
 package com.promiseeight.www.ui.setting
 
 import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -8,10 +10,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.promiseeight.www.BuildConfig
 import com.promiseeight.www.databinding.FragmentSettingBinding
 import com.promiseeight.www.ui.common.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers.Main
 
 @AndroidEntryPoint
 class SettingFragment : BaseFragment<FragmentSettingBinding>() {
@@ -32,6 +36,18 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>() {
             )
         }
 
+        binding.tvSettingFeedback.setOnClickListener {
+            UriParse("")
+        }
+
+        binding.tvSettingPrivateInfo.setOnClickListener {
+            UriParse("https://whenwherewhat.notion.site/WWW-d44308e244964fb7a12006efe2eb94cb") //클릭시 개인정보방침
+        }
+
+        binding.tvSettingOpenSource.setOnClickListener {
+            getOpenSource()
+        }
+
     }
 
     private fun getVersionInfo() = BuildConfig.VERSION_NAME
@@ -41,6 +57,19 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>() {
 //            val builder = AlertDialog.Builder(this.parentFragmentManager.getFragment(SettingFragment))
 
         }
+    }
 
+
+    private fun UriParse(urlGet : String) {
+        val url = urlGet
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
+    }
+
+    private fun getOpenSource() {
+        activity?.let{
+            val intent = Intent (it, OssLicensesMenuActivity::class.java)
+            it.startActivity(intent)
+        }
     }
 }
