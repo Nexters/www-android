ff3cdb0a850aadb753dc781bc490aad52a60dc3c
Jung SeokJoon <eshc.joon@gmail.com>
2023-03-01 13:58:59 +0900

    [mod] #12 - HomeMeeting 관련 UiModel 추가 및 수정

--- app/src/main/java/com/promiseeight/www/ui/model/MeetingUiModel.kt
+++ app/src/main/java/com/promiseeight/www/ui/model/MeetingUiModel.kt
@@ -1,14 +1,30 @@
 package com.promiseeight.www.ui.model
 
+import com.promiseeight.www.domain.model.MeetingMain
+import com.promiseeight.www.domain.model.MeetingStatus
+import com.promiseeight.www.ui.model.enums.MeetingYaksogi
+
 data class MeetingUiModel(
     val hostName : String,
-    val joinedUserCount : Long,
+    val joinedUserCount : Int,
     val meetingId : Long, // pk
     val meetingName : String,
-    val meetingStatus : String,
+    val meetingStatus : MeetingStatus,
     val minimumAlertMembers : Long,
-    val confirmedDate : String? = null,
-    val confirmedPlace : String? = null,
-    val confirmedTime : String? = null,
-    val votingUserCount : Int
+    val confirmedDate : String?,
+    val confirmedPlace : String?,
+    val confirmedTime : String?,
+    val votingUserCount : Int,
+    val yaksogi: MeetingYaksogi,
+    val dDay : String = ""
+)
+
+fun MeetingMain.toMeetingUiModel() = MeetingUiModel(
+    hostName = hostName,
+    joinedUserCount = joinedUserCount,
+    meetingId = meetingId,
+    meetingName,
+    meetingStatus = MeetingStatus.valueOf(meetingStatus)
+    , minimumAlertMembers, confirmedDate, confirmedPlace, confirmedTime, votingUserCount,
+    yaksogi = MeetingYaksogi.valueOf(yaksokiType)
 )
