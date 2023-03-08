be36ae995c74a460b488b4f9338ccf57edbede2c
Jung SeokJoon <eshc.joon@gmail.com>
2023-03-09 08:04:53 +0900

    [add] #83 BasicDialog 추가

---
+++ app/src/main/java/com/promiseeight/www/ui/common/util/DialogUtil.kt
@@ -0,0 +1,29 @@
+package com.promiseeight.www.ui.common.util
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+
+object DialogUtil {
+
+    //단순하게 title, content, ok, cancel 만 있는 다이얼로그를 부르는 함수
+    fun showBasicAlertDialog(
+        context : Context,
+        title : String = "",
+        content : String = "",
+        ok : () -> Unit = {},
+        okText : String = "확인",
+        cancel : () -> Unit = {},
+        cancelText : String = "취소",
+        cancelable : Boolean = true
+    ){
+        AlertDialog.Builder(context)
+            .setTitle(title)
+            .setMessage(content)
+            .setPositiveButton(okText){ _, _ ->
+                ok()
+            }.setNegativeButton(cancelText){ _, _ ->
+                cancel()
+            }.setCancelable(cancelable)
+            .show()
+    }
+}
